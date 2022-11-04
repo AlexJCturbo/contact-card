5bab48627a70f522aa21b4d10783ffadfc6d4d12
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-11-04 01:16:46 -0600

    bundle static files and import css and Bootstrap into webpack entry point

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,2 +1,21 @@
+//Import js modules
 import './form';
 import './submit'
+
+//Import css
+import '../css/index.css';
+
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Import images
+import Logo from '../images/contact-info_64px.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
+//Add images on load
+window.addEventListener('load', function () {
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,43 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/js/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      //Rule that directs JavaScript files to be handled by Babel. Notice that we also added a rule to exclude the node modules.
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+    })
+  ]
 }
