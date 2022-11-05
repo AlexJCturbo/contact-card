b37b633d17103617573b0c6a3b34bf562f3f4667
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-11-04 22:08:37 -0600

    indexedDB implemented

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,7 @@
 //Import js modules
 import './form';
 import './submit'
+import { initDb } from './database'
 
 //Import css
 import '../css/index.css';
@@ -15,6 +16,7 @@ import Dog from '../images/dog.png';
 
 //Add images on load
 window.addEventListener('load', function () {
+  initDb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
