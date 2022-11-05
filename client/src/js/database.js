fb96cb4a041e4fb95382b8b0e89e757fc1a098fb
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-11-05 00:23:16 -0600

    added read and create functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,3 +16,50 @@ export const initDb = async function () {
     }
   });
 };
+
+export const getDb = async function () {
+  console.log('GET from the database');
+  //Connect to the IndexedDB and version
+  const dbConnect = await openDB('contact_db', 1);
+
+  //Create a new transaction and specify the store and data privileges
+  const tx = dbConnect.transaction('contacts', 'readonly');
+
+  //Open up the desired object store
+  const store = tx.objectStore('contacts');
+
+  //Use .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  //Confirm the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+
+  // const contactDb = await openDB('library_db', 1.0);
+  // const tx = contactDb.transaction('transactionOne', 'readonly');
+  // const store = tx.objectStore('transactionOne');
+  // const request = store.getAll();
+  // const result = await request;
+  // return result;
+};
+
+export const postDb = async (name, email, phone, profile) => {
+  console.log('POST to the database');
+
+  //Create a connection to the database and specify the version we want to use
+  const dbConnect = await openDB('contact_db', 1);
+
+  //Create a new transaction and specify the store and data privileges
+  const tx = dbConnect.transaction('contacts', 'readwrite');
+
+  //Open up the desired object store
+  const store = tx.objectStore('contacts');
+
+  //Use .add() method on the store and pass in the content
+  const request = store.add({ name: name, email: email, phone: phone, profile: profile });
+
+  //Get confirmation of the request
+  const result = await request;
+  console.log('Data saved to the database', result);
+}
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,7 +1,7 @@
 //Import js modules
 import './form';
 import './submit'
-import { initDb } from './database'
+import { initDb, getDb, postDb } from './database'
 
 //Import css
 import '../css/index.css';
@@ -16,7 +16,13 @@ import Dog from '../images/dog.png';
 
 //Add images on load
 window.addEventListener('load', function () {
+
+  //We are temporarily placing getDb() and postDb() function calls here for testing. We will move them to another event listener later.
   initDb();
+
+  postDb('Toby', 'toby_da_pup@fluff.com', 5212345678, 'Dog');
+  getDb();
+
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
