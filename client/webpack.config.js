ce484d5179b6a50c0f79fe1e44da15910c701e15
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-11-06 16:47:12 -0600

    created a manifest.json file for the PWA

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,9 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 //const WorkboxPlugin = require('workbox-webpack-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+//const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -10,41 +12,38 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-
-      //Rule that directs JavaScript files to be handled by Babel. Notice that we also added a rule to exclude the node modules.
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-        }
-      }
-    ],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
       title: 'Webpack Plugin',
     }),
+    new WebpackPwaManifest({
+      name: 'Contact Cards Application',
+      short_name: 'Contact Cards',
+      description: 'Keep track of contacts.',
+      background_color: '#7eb4e2',
+      theme_color: '#7eb4e2',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/contact-info_512px.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+        {
+          src: path.resolve('src/images/contact-info_512px.png'),
+          size: '1024x1024',
+          destination: path.join('assets', 'icons'),
+          purpose: 'maskable'
+        }
+      ],
+    }),
     new InjectManifest({
       swSrc: './src/sw.js',
       swDest: 'service-worker.js',
     })
+
     //Instead of GenerateSW we will use InjectManifest
     // new WorkboxPlugin.GenerateSW({
     //   //Do not precache images
@@ -68,5 +67,30 @@ module.exports = {
     //     },
     //   }],
     // })
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      //Rule that directs JavaScript files to be handled by Babel. Notice that we also added a rule to exclude the node modules.
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        }
+      }
+    ],
+  },
 }
